b5db8ab1ad1c33a1b6cca308a93d180ed067a4da
Garen Torikian <gjtorikian@users.noreply.github.com>
2016-09-16 15:22:11 -0700

    Update URL to include `v`

--- Casks/hyperterm.rb
+++ Casks/hyperterm.rb
@@ -3,7 +3,7 @@ cask 'hyperterm' do
   sha256 '7add5e9f73c70a536b9a715c10410a45552f4586c90f75c0b07e8404e00f80b8'
 
   # github.com/zeit/hyperterm was verified as official when first introduced to the cask
-  url "https://github.com/zeit/hyperterm/releases/download/#{version}/hyperterm-#{version}-mac.zip"
+  url "https://github.com/zeit/hyperterm/releases/download/v#{version}/hyperterm-#{version}-mac.zip"
   appcast 'https://github.com/zeit/hyperterm/releases.atom',
           checkpoint: 'd7b5fed723c882c236104c7571d330fe81a4e17c5dc3a4793ac5829105c3933e'
   name 'HyperTerm'
